d0fffbfd69e7faf2291f3b335fd352b39ef8c014
emmabeanween <32688713+emmabeanween@users.noreply.github.com>
2021-01-28 13:00:35 -0800

    Create BookStore.js

---
+++ BookStore.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'React';
+
+class Bookstore extends React.Component {
+
+constructor(props);
+super(props);
+this.state = {bookStorage: []}
+
+
+render(){
+
+return(<div></div>);
+
+}}
